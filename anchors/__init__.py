4b945ee7d16ff021a9caa8579c4ddb99e1859c01
Fantomas42 <fantomas42@gmail.com>
2016-08-01 10:54:03 +0200

    Create anchors module

---
+++ anchors/__init__.py
@@ -0,0 +1,8 @@
+"""django-anchors"""
+__version__ = '0.1.dev0'
+__license__ = 'BSD License'
+
+__author__ = 'Fantomas42'
+__email__ = 'fantomas42@gmail.com'
+
+__url__ = 'https://github.com/Fantomas42/django-anchors'
